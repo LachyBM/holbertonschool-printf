21c4e2f060ba0ae0abe3b40f77b7dbe17889f0cb
bfrasholb <13065@holbertonstudents.com>
2026-03-17 04:07:23 +0000

    Autoupdate

---
+++ test1/printer.h
@@ -0,0 +1,83 @@
+#include "main.h"
+
+/**
+ *pchar-prints a character
+ *@c:a string
+ */
+
+int pchar(char *c)
+{
+  write(1, &c, 1);
+  return (1);
+}
+
+/**
+ * pint- prints the int value
+ * @num: format
+ * Return: VALUE of int from num
+ */
+
+int pint(int num)
+{
+  int length = 0;
+  char c;
+
+  if (num < 0)
+    {
+      write(1, "-", 1);
+      length++;
+      num = -num;
+    }
+  if (num >= 10)
+    length += pint(num / 10);
+  c = (num % 10) + '0';
+  write(1, &c, 1);
+  length++;
+
+  return (length);
+}
+
+/**
+ *pfloat-prints a double
+ *@args:arguments
+ */
+
+void pfloat(va_list args)
+{
+  double f = va_arg(args, double);
+  printf("%f", f);
+}
+
+/**
+ *pstring-prints a string or (nil) if string == NULL
+ *@string:a string
+ */
+
+int pstring(char *string)
+{
+  int length = 0;
+
+  while(string[length])
+    length++;
+  write(1, string, length);
+  return (length);
+}
+
+/**
+ *puint-prints an unsigned string
+ *@unum:unsigned int
+ */
+
+unsigned int puint(unsigned int unum)
+{
+  int length = 0;
+  char c;
+
+  if (unum >= 10)
+    length += _print_int(unum / 10);
+  c = (unum % 10) + '0';
+  write(1, &c, 1);
+  length++;
+
+  return (length);
+}
