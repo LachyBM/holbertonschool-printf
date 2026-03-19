c2b8124e1851c36d860a859a74c413817c265a03
bfrasholb <13065@holbertonstudents.com>
2026-03-19 04:31:15 +0000

    Autoupdate

--- includes.c
+++ includes.c
@@ -1,4 +1,22 @@
 /**
+ *Specifier Array
+ */
+char_check specchar[] = {
+  {'%', percent},
+  {'c', pchar},
+  {'s', pstring},
+  {'d', pint},
+  {'i', pint},
+  {'u', puint},
+  {'x', phexl},
+  {'X', phexu},
+  {'b', pbin},
+  {'o', poct},
+  {'p', paddr},
+  {'\0', NULL}
+};
+
+/**
  *Handler C Files
  */
 
@@ -23,21 +41,3 @@
 #include "helpers/print_binary.c"
 #include "helpers/print_oct.c"
 #include "helpers/print_addr.c"
-
-/**
- *Specifier Array
- */
-char_check specchar[] = {
-  {'%', percent},
-  {'c', pchar},
-  {'s', pstring},
-  {'d', pint},
-  {'i', pint},
-  {'u', puint},
-  {'x', phexl},
-  {'X', phexu},
-  {'b', pbin},
-  {'o', poct},
-  {'p', paddr},
-  {'\0', NULL}
-};
