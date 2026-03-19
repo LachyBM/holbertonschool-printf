30ac6f09086f486f3bd88ed338f49437a29d3707
bfrasholb <13065@holbertonstudents.com>
2026-03-19 00:41:06 +0000

    Checking Time

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+  va_list args;
+  int total = 0, i = 0, j;
+
+  char_check specchar[] = {
+    {'%', percent},
+    {'c', pchar},
+    {'s', pstring},
+    {'d', pint},
+    {'i', pint},
+  };
+  
+  va_start(args, format);
+
+  while (format && format[i])
+    {
+      if (format[i] == '%')
+	{
+	  i++;
+	  j = 0;
+	  while (specchar[j].symbol)
+	    {
+	      if (format[i] == specchar[j].symbol)
+		{
+		  total += specchar[j].helper(args);
+		  break;
+		}
+	      j++;
+	    }
+	}
+      else
+	{
+	  write (1, &format[i], 1);
+	  total++;
+	}
+      i++;
+    }
+  va_end(args);
+  return (total);
+}
