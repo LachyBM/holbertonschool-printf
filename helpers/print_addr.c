ebc919e94d3e60cd906c5f29ebe0f50a46f71ac3
bfrasholb <13065@holbertonstudents.com>
2026-03-19 04:22:17 +0000

    Autoupdate

--- helpers/print_addr.c
+++ helpers/print_addr.c
@@ -8,6 +8,8 @@ int print_addr(void *addr)
   int count = 0;
   unsigned long paddr = (unsigned long)addr;
 
+  if (paddr == NULL)
+    return (-1);
   write(1, "0x",2);
   count += 2;
   count += print_hex(paddr, 0);
