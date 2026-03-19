a1f8896ec578358e9bc939f12a9776fbc2b28bba
bfrasholb <13065@holbertonstudents.com>
2026-03-19 02:29:45 +0000

    Autoupdate

--- test1/printer.c
+++ test1/printer.c
@@ -1,61 +1,77 @@
 #include "main.h"
 
-/* Print a single character */
+/**
+ *Print a Char
+ */
 int pchar(va_list args)
 {
   char c = va_arg(args, int);
   return write(1, &c, 1);
 }
 
-/* Print a string */
+/**
+ *Print a Char *
+ */
 int pstring(va_list args)
 {
   char *str = va_arg(args, char *);
   int len = 0;
-
+  
   if (!str)
     str = "(null)";
-
+  
   while (str[len])
     len++;
   write(1, str, len);
   return len;
 }
 
-/* Print signed integer */
+/**
+ *Print signed integer
+ */
 int pint(va_list args)
 {
-  int num = va_arg(args, int);
-  char buf[20];
-  int i = 0, len = 0;
-  unsigned int n;
-
+  int n = va_arg(args, int);
+  int count = 0;
+  long num = n;
+  
   if (num < 0)
     {
       write(1, "-", 1);
-      len++;
-      n = -num;
+      count++;
+      num = -num;
     }
-  else
-    n = num;
-
-  /* Convert number to string in reverse */
-  do {
-    buf[i++] = (n % 10) + '0';
-    n /= 10;
-  } while (n);
-
-  /* Print in correct order */
-  while (--i >= 0)
-    {
-      write(1, &buf[i], 1);
-      len++;
-    }
-  return len;
+  
+  if (num / 10)
+    count += print_number(num / 10);
+  
+  count += print_digit(num % 10);
+  return (count);
 }
 
+/**
+ *Print a percent literal
+ */
 int percent(va_list args)
 {
   (void)args;
   return write(1, "%", 1);
+}
+
+/**
+ *Print an unsigned int
+ */
+int puint(va_list args)
+{
+  unsigned int n = va_arg(args, unsigned int);
+  return (print_uns(n));
+}
+
+/**
+ *Print a Hex int
+ */
+int phex(va_list args)
+{
+  unsigned int n = va_arg(args, unsigned int);
+  return (print_hex(n, 0));
 }
