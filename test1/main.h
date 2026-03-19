7dc3b937fc2a72efe8430afdf1fa53ea52fadb6c
bfrasholb <13065@holbertonstudents.com>
2026-03-19 02:29:35 +0000

    Autoupdate

--- test1/main.h
+++ test1/main.h
@@ -8,20 +8,25 @@
 int _printf(const char *format, ...);
 
 /**
- * Helper Functions
+ * Handler Functions - Held in Specchar Struct
  */
 
 int pchar(va_list args);
 int pstring(va_list args);
 int pint(va_list args);
 int percent(va_list args);
-
+int puint(va_list args);
+int phex(va_list args);
+	  
 /**
- * Escape
+ * Print_Numbers Functions
  */
 
-/** int escape(char c); */
-
+int print_digit(int d);
+int print_number(long n);
+int print_uns(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
+	  
 /**
  * Struct Mapping Specifiers to Handlers
  */
