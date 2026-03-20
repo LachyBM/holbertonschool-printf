bb303ac036a0470cde950b054b9966c522248689
bfrasholb <13065@holbertonstudents.com>
2026-03-20 16:48:55 +1100

    Cleaning

--- handlers/phexu.c
+++
@@ -1,9 +0,0 @@
-#include "../main.h"
-/**
- *Print an uppercase Hex int
- */
-int phexu(va_list args)
-{
-  unsigned int n = va_arg(args, unsigned long int);
-  return (print_hex(n, 1));
-}
--- handlers/poct.c
+++
@@ -1,9 +0,0 @@
-#include "../main.h"
-/**
- *Print an Oct
- */
-int poct(va_list args)
-{
-  unsigned int n = va_arg(args, unsigned int);
-  return (print_oct(n));
-}
